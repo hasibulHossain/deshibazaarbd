69025cbd9cc84aeba884e4c2293ad8f5f6be7752
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-03-31 07:20:06 +0600

    stripe payment 50% done

--- components/layouts/page/CheckoutPayment/PayMethod/MasterCard/SpliteCardForm.js
+++ components/layouts/page/CheckoutPayment/PayMethod/MasterCard/SpliteCardForm.js
@@ -5,19 +5,16 @@ import {
     CardNumberElement,
     CardCvcElement,
     CardExpiryElement,
-    CardElement
 } from "@stripe/react-stripe-js";
 import Axios from "axios";
-
-// import useResponsiveFontSize from "../../useResponsiveFontSize";
+import { showToast } from "../../../../../master/Helper/ToastHelper";
 
 const useOptions = () => {
-    //   const fontSize = useResponsiveFontSize();
     const options = useMemo(
         () => ({
             style: {
                 base: {
-                    fontSize: "16px",
+                    fontSize: 14,
                     color: "#424770",
                     letterSpacing: "0.025em",
                     fontFamily: "Source Code Pro, monospace",
@@ -40,6 +37,8 @@ const SpliteCardForm = () => {
     const stripe = useStripe();
     const elements = useElements();
     const options = useOptions();
+    const [error, setError] = useState(null);
+
 
     const [billingDetails, setBillingDetails] = useState({
         email: "m.akash.cse@gmail.com",
@@ -51,13 +50,16 @@ const SpliteCardForm = () => {
         event.preventDefault();
 
         if (!stripe || !elements) {
-            // Stripe.js has not loaded yet. Make sure to disable
-            // form submission until Stripe.js has loaded.
             return;
         }
 
         const card = elements.getElement(CardNumberElement);
         const result = await stripe.createToken(card);
+        // const result = await stripe.createToken({
+        //     type: 'card',
+        //     card,
+        //     billingDetails
+        // });
 
         // const payload = await stripe.createPaymentMethod({
         //     type: "card",
@@ -65,49 +67,15 @@ const SpliteCardForm = () => {
         // });
         // console.log("[PaymentMethod]", payload);
 
-
         if (result.error) {
-            // Show error to your customer.
-            console.log(result.error.message);
+            alert(result.error.message);
         } else {
-            // Send the token to your server.
-            // This function does not exist yet; we will define it in the next step.
             stripeTokenHandler(result.token);
         }
-
-        // const payload = await stripe.createPaymentMethod({
-        //     type: "card",
-        //     card: elements.getElement(CardNumberElement),
-        //     billing_details: billingDetails
-        // });
-
-        // hit the payment api
-        // if (payload) {
-        //     const data = {
-        //         order_id: 2,
-        //         payload: payload.paymentMethod
-        //     }
-
-        //     Axios.post(`${process.env.NEXT_PUBLIC_API_URL}payments`, data)
-        //         .then(res => {
-        //             console.log(`res after payment`, res);
-        //         })
-        // }
-
-
-        // console.log("payload payment", payload);
     };
 
     function stripeTokenHandler(token) {
         const paymentData = { token: token.id };
-
-        // const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}payments`, {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify(paymentData),
-        // });
 
         const data = {
             order_id: 2,
@@ -116,11 +84,13 @@ const SpliteCardForm = () => {
 
         Axios.post(`${process.env.NEXT_PUBLIC_API_URL}payments`, data)
             .then(res => {
+                if (res.data.status) {
+                    showToast('success', res.data.message);
+                } else {
+                    showToast('error', res.data.message);
+                }
                 console.log(`res after payment`, res);
             })
-
-        // Return and display the result of the charge.
-        // return response.json();
     }
 
     return (
@@ -128,60 +98,19 @@ const SpliteCardForm = () => {
             <form onSubmit={handleSubmit}>
                 <label>
                     Card number
-                    <CardNumberElement
-                        options={options}
-                        onReady={() => {
-                            console.log("CardNumberElement [ready]");
-                        }}
-                        onChange={event => {
-                            console.log("CardNumberElement [change]", event);
-                        }}
-                        onBlur={() => {
-                            console.log("CardNumberElement [blur]");
-                        }}
-                        onFocus={() => {
-                            console.log("CardNumberElement [focus]");
-                        }}
-                    />
+                    <CardNumberElement options={options} />
                 </label>
                 <label>
                     Expiration date
-                    <CardExpiryElement
-                        options={options}
-                        onReady={() => {
-                            console.log("CardNumberElement [ready]");
-                        }}
-                        onChange={event => {
-                            console.log("CardNumberElement [change]", event);
-                        }}
-                        onBlur={() => {
-                            console.log("CardNumberElement [blur]");
-                        }}
-                        onFocus={() => {
-                            console.log("CardNumberElement [focus]");
-                        }}
-                    />
+                    <CardExpiryElement options={options} />
                 </label>
                 <label>
                     CVC
-                    <CardCvcElement
-                        options={options}
-                        onReady={() => {
-                            console.log("CardNumberElement [ready]");
-                        }}
-                        onChange={event => {
-                            console.log("CardNumberElement [change]", event);
-                        }}
-                        onBlur={() => {
-                            console.log("CardNumberElement [blur]");
-                        }}
-                        onFocus={() => {
-                            console.log("CardNumberElement [focus]");
-                        }}
-                    />
+                    <CardCvcElement options={options} />
                 </label>
+
                 <button type="submit" className="btn custom-pay-btn" disabled={!stripe}>
-                    Pay
+                    Pay Now
                 </button>
             </form>
         </div>
