0aff84e72584139d51d65eb088de845b986da7db
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-05-07 22:48:43 +0600

    Added Demo Notice Bar at top

---
+++ components/Demo/DemoWarning.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { faBell } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const DemoWarning = () => {
+    return (
+        <div className="alert alert-info p-3 text-center mb-0">
+            <h2 className="font-weight-lighter">
+                <FontAwesomeIcon className="custome-fontAwesome" icon={faBell} /> {" "}
+                Hello Sir, Site is under development. 
+                Please wait & we'll do business together !
+            </h2>
+        </div>
+    );
+}
+
+export default DemoWarning;
--- components/layouts/MainLayout.js
+++ components/layouts/MainLayout.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
+import DemoWarning from "../Demo/DemoWarning";
 
 const MainLayout = (props) => {
   return (
     <>
+      <DemoWarning />
       <Header />
       {props.children}
       <Footer/>
