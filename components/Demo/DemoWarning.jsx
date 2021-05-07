43cadec49a3fa21f9bbf3e725f6b47aee7e0b4bd
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-05-07 23:11:40 +0600

    Updated Site Demo Notice

--- components/Demo/DemoWarning.jsx
+++ components/Demo/DemoWarning.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 
-import { faBell } from '@fortawesome/free-solid-svg-icons';
+import { faBell, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const styleDemoText = {
+    color: 'red',
+    fontStyle: 'italic',
+    fontSize: '1.4rem',
+    fontWeight: 'bold!important'
+}
 
 const DemoWarning = () => {
     return (
-        <div className="alert alert-info p-3 text-center mb-0">
-            <h2 className="font-weight-lighter">
+        <div className="alert alert-info p-3 text-center mb-0" >
+            <h2 className="font-weight-lighter" style={ styleDemoText }>
                 <FontAwesomeIcon className="custome-fontAwesome" icon={faBell} size={20} /> {" "}
                 Hello Sir, Site is under development. 
-                Please wait & we'll do business together !
+                Please be patient & we'll do business together 
+                {" "}<FontAwesomeIcon className="custome-fontAwesome" color="green" icon={faCheckCircle} size={20} /> {" "}
+                <FontAwesomeIcon className="custome-fontAwesome" color="green" icon={faCheckCircle} size={20} /> {" "}
             </h2>
         </div>
     );
