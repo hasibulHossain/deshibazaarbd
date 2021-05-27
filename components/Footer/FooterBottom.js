212046e97c9b977015043fe48d1fe5b310fd0624
ma-foyeze <mafoyez.bd@gmail.comm>
2021-05-27 22:53:58 +0600

    Page design updated

--- components/Footer/FooterBottom.js
+++ components/Footer/FooterBottom.js
@@ -5,14 +5,14 @@ const FooterBottom = () => {
     return (
         <div className="footer-bottom">
             <Container>
-                <div className="row justify-content-between">
+                <div className="row justify-content-between align-items-center">
                     <div className="col-md-6">
                         <div className="footer-bottom-info">
                             <p> &#169; {new Date().getFullYear()} deshibazaar. | deshibazaar.net <span>All rights reserved</span></p>
                         </div>
                     </div>
                     <div className="col-md-4">
-
+                        <img src={"/images/Payment-Icon-Buzfi.com-best-online-shopping-in-the-USA-2020-best-e-commerce-shop-in-the-United-States.png"} alt="deshi bazaar bd" className="img-fluid mb-2 mt-2 pointer" />
                     </div>
                 </div>
             </Container>
