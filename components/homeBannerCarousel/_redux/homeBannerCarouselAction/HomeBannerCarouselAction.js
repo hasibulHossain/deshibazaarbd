378c2a5547b202923c6def3e801b62ca81d52112
hasib <hasibulhossain52@gmail.com>
2021-06-13 22:33:37 +0600

    binding carousel api with frontend

--- components/homeBannerCarousel/_redux/homeBannerCarouselAction/HomeBannerCarouselAction.js
+++ components/homeBannerCarousel/_redux/homeBannerCarouselAction/HomeBannerCarouselAction.js
@@ -1,48 +1,50 @@
-import Axios from 'axios';
+import Axios from "axios";
 import * as Types from "../Type/Types";
 
-export const getHomeCarouselData = () => (dispatch) => {
-    // const responseData = {
-    //     data: [],
-    //     status: false,
-    //     isLoading: true,
-    // }
-    // dispatch({type: Types.GET_BANNER_CAROUSEL_LIST, payload: responseData});
-    //this data is use for only test
-    const data = [
-        {
-            title: "Shopping as you love",
-            discountType: "Winter Sale",
-            dicountRate: "50%",
-            ProuctLink: "https://www.google.com",
-            BannerURL: "https://deshibazaarbd.com/images/slider/01-Slider.jpeg"
-        },
-        {
-            title: "Shopping as you love",
-            discountType: "Winter Sale",
-            dicountRate: "50%",
-            ProuctLink: "https://www.google.com",
-            BannerURL: "https://deshibazaarbd.com/images/slider/02-Slider.jpeg"
-        },
-        {
-            title: "Shopping as you love",
-            discountType: "Winter Sale",
-            dicountRate: "50%",
-            ProuctLink: "https://www.google.com",
-            BannerURL: "https://deshibazaarbd.com/images/slider/03-Slider.jpeg"
-        },
-        {
-            title: "Shopping as you love",
-            discountType: "Winter Sale",
-            dicountRate: "50%",
-            ProuctLink: "https://www.google.com",
-            BannerURL: "https://deshibazaarbd.com/images/slider/04-Slider.jpeg"
-        },
-    ]
-    const responseData = {
-        data: data,
-        status: true,
-        isLoading: false,
-    }
-    dispatch({type: Types.GET_BANNER_CAROUSEL_LIST, payload: responseData});
-}
+export const getHomeCarouselData = () => async (dispatch) => {
+  const data = [
+    {
+      title: "Shopping as you love",
+      discountType: "Winter Sale",
+      dicountRate: "50%",
+      ProuctLink: "https://www.google.com",
+      BannerURL: "https://deshibazaarbd.com/images/slider/01-Slider.jpeg",
+    },
+    {
+      title: "Shopping as you love",
+      discountType: "Winter Sale",
+      dicountRate: "50%",
+      ProuctLink: "https://www.google.com",
+      BannerURL: "https://deshibazaarbd.com/images/slider/02-Slider.jpeg",
+    },
+    {
+      title: "Shopping as you love",
+      discountType: "Winter Sale",
+      dicountRate: "50%",
+      ProuctLink: "https://www.google.com",
+      BannerURL: "https://deshibazaarbd.com/images/slider/03-Slider.jpeg",
+    },
+    {
+      title: "Shopping as you love",
+      discountType: "Winter Sale",
+      dicountRate: "50%",
+      ProuctLink: "https://www.google.com",
+      BannerURL: "https://deshibazaarbd.com/images/slider/04-Slider.jpeg",
+    },
+  ];
+  console.log("url => ", `${process.env.NEXT_PUBLIC_API_URL}sliders-frontend`);
+  try {
+    dispatch({ type: Types.INIT_BANNER_CAROUSEL_LIST });
+    const res = await Axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}sliders-frontend`
+    );
+    const data = res.data.data;
+    dispatch({
+      type: Types.GET_BANNER_CAROUSEL_LIST,
+      payload: { carouselList: data },
+    });
+  } catch (error) {
+    console.log("homebanner carousel error ", error);
+    dispatch({ type: Types.FETCH_BANNER_CAROUSEL_LIST_FAILED });
+  }
+};
