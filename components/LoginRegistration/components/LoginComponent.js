993a75222602a41206edd4dde352646afd1768fb
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-06-14 21:52:01 +0600

    Updated Login error

--- components/LoginRegistration/components/LoginComponent.js
+++ components/LoginRegistration/components/LoginComponent.js
@@ -25,111 +25,118 @@ const LoginComponent = () => {
         dispatch(loginAction(loginInput));
         // e.preventDefault();
     }
-    const router = useRouter()
+
+    const router = useRouter();
+
     if (isLogging === true) {
         router.push('/profile')
         dispatch(emptyDispatch())
     }
+
     return (
         <>
             <div className="account_info_body mt-5">
-                <form
-                    onSubmit={handleSubmit(handleLogin)}
-                    method="post"
-                    autoComplete="off"
-                    encType="multipart/form-data">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <div class="mb-3">
-                                <label for="lastName" class="form-label">Email Or Phone Number</label>
-                                <input
-                                    type="text"
-                                    class="form-control"
-                                    placeholder=""
-                                    name="email"
-                                    value={loginInput.email}
-                                    onChange={(e) => handleLoginInputChange('email', e.target.value)}
-                                    ref={register({
-                                        required: true,
-                                        maxLength: 100,
-                                    })}
-                                />
-                                {errors.email && errors.email.type === 'required' && (
-                                    <ErrorMessage errorText="Email or Phone can't be blank!" />
-                                )}
+                {
+                    typeof loginInput !== 'undefined' &&
+                    <form
+                        onSubmit={handleSubmit(handleLogin)}
+                        method="post"
+                        autoComplete="off"
+                        encType="multipart/form-data">
+                        <div className="row">
+                            <div className="col-md-6">
+                                <div class="mb-3">
+                                    <label for="lastName" class="form-label">Email Or Phone Number</label>
+                                    <input
+                                        type="text"
+                                        class="form-control"
+                                        placeholder=""
+                                        name="email"
+                                        value={loginInput.email}
+                                        onChange={(e) => handleLoginInputChange('email', e.target.value)}
+                                        ref={register({
+                                            required: true,
+                                            maxLength: 100,
+                                        })}
+                                    />
+                                    {errors.email && errors.email.type === 'required' && (
+                                        <ErrorMessage errorText="Email or Phone can't be blank!" />
+                                    )}
 
-                            </div>
-                        </div>
-
-                        <div className="col-md-6">
-                            <label for="password" class="form-label">Password</label>
-                            <div class="account_input_group">
-                                <input
-                                    type={showPassword === true ? "text" : "password"} class="form-control" id="inlineFormInputGroup" placeholder=""
-                                    name="password"
-                                    value={loginInput.password}
-                                    onChange={(e) => handleLoginInputChange('password', e.target.value)}
-                                    ref={register({
-                                        required: true,
-                                        maxLength: 100,
-                                    })}
-                                />
-                                {errors.password && errors.password.type === 'required' && (
-                                    <ErrorMessage errorText="Password can't be blank!" />
-                                )}
-
-                                <div class="account_input_group_prepend" onClick={() => setShowPassword(!showPassword)}>
-                                    {
-                                        showPassword === true ? <FontAwesomeIcon icon={faEyeSlash} /> : <FontAwesomeIcon icon={faEye} />
-                                    }
                                 </div>
                             </div>
+
+                            <div className="col-md-6">
+                                <label for="password" class="form-label">Password</label>
+                                <div class="account_input_group">
+                                    <input
+                                        type={showPassword === true ? "text" : "password"} class="form-control" id="inlineFormInputGroup" placeholder=""
+                                        name="password"
+                                        value={loginInput.password}
+                                        onChange={(e) => handleLoginInputChange('password', e.target.value)}
+                                        ref={register({
+                                            required: true,
+                                            maxLength: 100,
+                                        })}
+                                    />
+                                    {errors.password && errors.password.type === 'required' && (
+                                        <ErrorMessage errorText="Password can't be blank!" />
+                                    )}
+
+                                    <div class="account_input_group_prepend" onClick={() => setShowPassword(!showPassword)}>
+                                        {
+                                            showPassword === true ? <FontAwesomeIcon icon={faEyeSlash} /> : <FontAwesomeIcon icon={faEye} />
+                                        }
+                                    </div>
+                                </div>
+                            </div>
+
                         </div>
 
-                    </div>
-
-                    <div className="d-flex justify-content-between">
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="" id="loginRememberMe" />
-                            <label class="account_info_label pointer" for="loginRememberMe">
-                                Remember me
-                            </label>
+                        <div className="d-flex justify-content-between">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" value="" id="loginRememberMe" />
+                                <label class="account_info_label pointer" for="loginRememberMe">
+                                    Remember me
+                                </label>
+                            </div>
+                            <p className="forget_password_link">
+                                <Link href="/">
+                                    <a>Forget your password?</a>
+                                </Link>
+                            </p>
                         </div>
-                        <p className="forget_password_link">
-                            <Link href="/">
-                                <a>Forget your password?</a>
-                            </Link>
-                        </p>
-                    </div>
 
-                    <div className="account_btn_group">
-                        {/* <button className="btn account_btn mt-2">SIGN IN</button> */}
+                        <div className="account_btn_group">
+                            {/* <button className="btn account_btn mt-2">SIGN IN</button> */}
 
-                        {
-                            !isLogging && (
-                                <button className="btn account_btn mt-2">SIGN IN</button>
-                            )
-                        }
-                        {
-                            isLogging && (
-                                <button disabled={true} type="submit" className="btn account_btn mt-2">
-                                    <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>    Logging
-                                </button>
-                                //         <a>
-                                //             {" "}
-                                //             <Button disabled={true} variant="primary" type="submit">
-                                //                 <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Login
-                                // </Button>
-                                //         </a>
-                            )
-                        }
+                            {
+                                !isLogging && (
+                                    <button className="btn account_btn mt-2">SIGN IN</button>
+                                )
+                            }
+                            {
+                                isLogging && (
+                                    <button disabled={true} type="submit" className="btn account_btn mt-2">
+                                        <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>    Logging
+                                    </button>
+                                    //         <a>
+                                    //             {" "}
+                                    //             <Button disabled={true} variant="primary" type="submit">
+                                    //                 <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Login
+                                    // </Button>
+                                    //         </a>
+                                )
+                            }
 
 
-                        <p className="mt-2">or Sign up with</p>
-                        <button className="btn google_btn mr-3 mt-2"><FontAwesomeIcon className="mr-2" icon={faGoogle} />Google</button>
-                        <button className="btn facebook_btn mt-2"><FontAwesomeIcon className="mr-2" icon={faFacebookF} />Google</button>
-                    </div>
-                </form>
+                            <p className="mt-2">or Sign up with</p>
+                            <button className="btn google_btn mr-3 mt-2"><FontAwesomeIcon className="mr-2" icon={faGoogle} />Google</button>
+                            <button className="btn facebook_btn mt-2"><FontAwesomeIcon className="mr-2" icon={faFacebookF} />Google</button>
+                        </div>
+                    </form>
+                }
+                
 
                 <p className="already_account">
                     Don't have an account?
