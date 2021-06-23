d0121737b602a718adfeaaf7b0b839cbcb4a6e59
hasib <hasibulhossain52@gmail.com>
2021-06-24 00:29:17 +0600

    change productList name to ProductListReducer

--- _redux/RootReducer.js
+++ _redux/RootReducer.js
@@ -22,7 +22,7 @@ import SearchReducer from "../components/SearchInput/_redux/Reducer/SearchInputR
 import DeliveryInfoReducer from "../components/Delivery/_redux/Reducer/DeliveryInfoReducer";
 import ProfileUpdateReducer from "../components/myprofile/_redux/Reducer/ProfileUpdateReducer";
 import PaymentMethodReducer from "../components/PaymentMethod/_redux/Reducer/PaymentMethodReducer";
-import ProductList from "../components/ProductList/_redux/Reducer/ProductListReducer";
+import ProductListReducer from "../components/ProductList/_redux/Reducer/ProductListReducer";
 
 export default combineReducers({
   GlobalReducer: GlobalReducer,
@@ -47,5 +47,5 @@ export default combineReducers({
   DeliveryInfoReducer: DeliveryInfoReducer,
   ProfileUpdateReducer: ProfileUpdateReducer,
   PaymentMethodReducer: PaymentMethodReducer,
-  ProductListReducer: ProductList,
+  ProductListReducer: ProductListReducer,
 });
