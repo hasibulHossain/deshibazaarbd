990fc6123f7078b9245bf487a1d9732a0f5067ba
Fabliha Afia <afia.corp@akij.net>
2021-06-25 18:05:30 +0600

    Updated some design and added links to product page

--- components/CategoryWishProductList/CategoryWishProductList.js
+++ components/CategoryWishProductList/CategoryWishProductList.js
@@ -6,21 +6,23 @@ import CategoryWiseMiniProduct from "./CategoryWiseMiniProduct";
 import { useSelector } from "react-redux";
 
 const CategoryWishProductList = () => {
+
   const { isLoading } = useSelector(
     (state) => state.CategoryWiseProductReducer
   );
+
   const [columns, setColumns] = useState("col-md-3");
 
   return (
     <section className="category_wise_product_list">
       <div className="row justify-content-between">
         <div className="col-md-5">
-          <h5>We've got 50 products for you..</h5>
+          <h5>We've got 0 products for you..</h5>
         </div>
         <div className="col-md-5">
           <div className="d-flex justify-content-end">
             <div className="filter_view">
-              <span>view :</span>
+              <span>View :</span>
               <FontAwesomeIcon
                 className={
                   columns == "col-md-3"
@@ -32,18 +34,18 @@ const CategoryWishProductList = () => {
               />
               <FontAwesomeIcon
                 className={
-                  columns == "col-md-10"
+                  columns == "col-md-12"
                     ? "filter_columns column_active"
                     : "filter_columns"
                 }
                 icon={faList}
-                onClick={() => setColumns("col-md-10")}
+                onClick={() => setColumns("col-md-12")}
               />
             </div>
             <div className="filter_view d-flex ml-3">
-              <span>sort by :</span>
+              <span>Sort by :</span>
               <Form>
-                <Form.Group controlId="exampleForm.SelectCustom">
+                <Form.Group controlId="exampleFormSelectCustom">
                   <Form.Control as="select" custom>
                     <option>Price</option>
                     <option>Rating</option>
--- components/ShopByCategory/ShopByCategoryContainer.js
+++ components/ShopByCategory/ShopByCategoryContainer.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Button from '../master/Button/Button';
-import ProductList from './ProductList';
+import CategoryListHome from './CategoryListHome';
 
 const ShopByCategoryContainer = () => {
     return (
         <section className="product-container">
             <div className="product-heading">
-                <h5>shop by categories</h5>
+                <h5>Shop by categories</h5>
                 <Button buttonText="view all" isFontAwesome={true} />
             </div>
-            <ProductList />
+            <CategoryListHome />
         </section>
     );
 };
