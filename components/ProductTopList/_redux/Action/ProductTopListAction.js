bc7ed4d8860e9649c5269a94e7dc8b5e713857e1
hasib <hasibulhossain52@gmail.com>
2021-07-04 23:44:53 +0600

    updated hot deals

--- components/ProductTopList/_redux/Action/ProductTopListAction.js
+++ components/ProductTopList/_redux/Action/ProductTopListAction.js
@@ -1,6 +1,8 @@
 import Axios from 'axios';
 import * as Types from "../Type/Types";
 
+const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+
 export const getTopProductList = () => (dispatch) => {
     // const responseData = {
     //     data: [],
@@ -46,9 +48,20 @@ export const getTopProductList = () => (dispatch) => {
         
     ]
     const responseData = {
-        data: data,
+        data: [],
         status: true,
         isLoading: false,
     }
     dispatch({type: Types.GET_TOP_PRODUCTS_LIST, payload: responseData});
+
+    Axios.get(`${baseUrl}get-items?type=hot-deal&paginate_no=5`)
+    .then(res => {
+        responseData.data = res.data.data.data;
+        responseData.isLoading = false;
+        dispatch({type: Types.GET_TOP_PRODUCTS_LIST, payload: responseData});
+    })
+    .catch(err => {
+        console.log('hot deals err => ', err)
+    })
+
 }
