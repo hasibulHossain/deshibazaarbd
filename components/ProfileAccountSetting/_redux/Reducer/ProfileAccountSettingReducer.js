0685ff50428762951f118e470508f234d759157d
ma-foyez <mafoyez.bd@gmail.com>
2021-07-13 01:13:45 +0600

    user profile userdata updated

--- components/ProfileAccountSetting/_redux/Reducer/ProfileAccountSettingReducer.js
+++ components/ProfileAccountSetting/_redux/Reducer/ProfileAccountSettingReducer.js
@@ -1,79 +1,89 @@
 import * as Types from "./../Type/Types";
 
 const initialState = {
-    isLoading: false,
-    isSubmitting: false,
-    bestSellerList: [],
-    shippingAddress: [],
-    billingAddress: [],
-    userInputData: {
-        first_name: null,
-        surname: null,
-        last_name: null,
-        email: null,
-        username: null,
-        phone_no: null,
-        password: '123456',
-        language: "en",
-        avatar: null,
-        banner: null,
-        address: null
+    isLoading           : false,
+    isSubmitting        : false,
+    bestSellerList      : [],
+    shippingAddress     : [],
+    billingAddress      : [],
+    userInputData       : {
+        first_name      : null,
+        surname         : null,
+        last_name       : null,
+        email           : null,
+        username        : null,
+        phone_no        : null,
+        password        : '123456',
+        language        : "en",
+        avatar          : null,
+        banner          : null,
+        address         : null
     },
-    billingAddressInput: {
-        type: "billing_address",
-        user_id: null,
-        transaction_id: null,
-        country_id: null, //integer
-        country: null,
-        city_id: null,  //integer
-        city: null,
-        area_id: null,   //integer
-        area: null,
-        street1: null,
-        street2: null,
-        is_default: 1
+    billingAddressInput : {
+        type            : "billing_address",
+        user_id         : null,
+        transaction_id  : null,
+        country_id      : null, //integer
+        country         : null,
+        city_id         : null,  //integer
+        city            : null,
+        area_id         : null,   //integer
+        area            : null,
+        street1         : null,
+        street2         : null,
+        is_default      : 1
     },
     shippingAddressInput: {
-        type: "shipping_address",
-        user_id: null,
-        transaction_id: null,
-        country_id: null, //integer
-        country: null,
-        city_id: null,  //integer
-        city: null,
-        area_id: null,   //integer
-        area: null,
-        street1: null,
-        street2: null,
-        is_default: 1
+        type            : "shipping_address",
+        user_id         : null,
+        transaction_id  : null,
+        country_id      : null, //integer
+        country         : null,
+        city_id         : null,  //integer
+        city            : null,
+        area_id         : null,   //integer
+        area            : null,
+        street1         : null,
+        street2         : null,
+        is_default      : 1
     },
-    countryList: [],
-    cityList: [],
-    areaList: [],
-
+    countryList         : [],
+    cityList            : [],
+    areaList            : [],
+    userDetails         : null,
 }
 function ProfileAccountSettingReducer(state = initialState, action) {
     switch (action.type) {
         case Types.GET_SHIPPING_ADDRESS:
             return {
                 ...state,
-                isLoading: action.payload.isLoading,
+                isLoading      : action.payload.isLoading,
                 shippingAddress: action.payload.data,
             }
         case Types.GET_BILLING_ADDRESS:
             return {
                 ...state,
-                isLoading: action.payload.isLoading,
+                isLoading     : action.payload.isLoading,
                 billingAddress: action.payload.data,
             }
         case Types.GET_USER_UPDATED_DATA:
-            console.log('action.payload :>> ', action.payload);
-            let getUserInput = { ...state.userInputData };
-            getUserInput = action.payload;
             return {
                 ...state,
-                userInputData: getUserInput,
+                userInputData: action.payload,
+                userDetails  : action.payload,
             }
+        case Types.CHANGE_USER_INPUT_DATA:
+            const userInputData = { ...state.userInputData };
+            userInputData[action.payload.name] = action.payload.value
+            return {
+                ...state,
+                userInputData
+            };
+        case Types.UPDATED_USER_DATA:
+            return {
+                ...state,
+                isSubmitting: action.payload.isLoading,
+            };
         case Types.CHANGE_BILLING_ADDRESS_INPUT:
             const billingAddressInput = { ...state.billingAddressInput };
             billingAddressInput[action.payload.name] = action.payload.value
@@ -121,7 +131,7 @@ function ProfileAccountSettingReducer(state = initialState, action) {
             if (action.payload.status == true) {
                 return {
                     ...state,
-                    isSubmitting: action.payload.isLoading,
+                    isSubmitting       : action.payload.isLoading,
                     billingAddressInput: initialState.billingAddressInput
 
                 };
