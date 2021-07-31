581ceeda3ef5259ff9dbb3f6b23821bdbf3ab6ee
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-07-31 13:28:20 +0600

    Basic dataset added for translation

---
+++ services/translation/dataset.js
@@ -0,0 +1,136 @@
+export default [
+    {
+        'key': 'Get Your Holiday Shopping On Wholesale Pricing',
+        'en' : 'Get Your Holiday Shopping On Wholesale Pricing',
+        'bn' : 'হলিডে শিপিং এবং খরচ'
+    },
+    {
+        'key': 'Become a Seller',
+        'en' : 'Become a Seller',
+        'bn' : 'সেলার রেজিস্ট্রেশন করুন'
+    },
+    {
+        'key': 'Track My Order',
+        'en' : 'Track My Order',
+        'bn' : 'অর্ডার ট্র্যাক করুন'
+    },
+    {
+        'key': 'Language',
+        'en' : 'Language',
+        'bn' : 'ভাষা'
+    },
+    {
+        'key': 'All Categories',
+        'en' : 'All Categories',
+        'bn' : 'সমস্ত ক্যাটাগরি'
+    },
+    {
+        'key': 'Search for Products, Brands or more',
+        'en' : 'Search for Products, Brands or more',
+        'bn' : 'প্রোডাক্ট, ব্র্যান্ড, ক্যাটাগরি সার্চ করুন'
+    },
+    {
+        'key': 'Sign In',
+        'en' : 'Sign In',
+        'bn' : 'সাইন ইন করুন'
+    },
+    {
+        'key': 'Sign Up',
+        'en' : 'Sign Up',
+        'bn' : 'সাইন আপ করুন'
+    },
+    {
+        'key': 'My Account',
+        'en' : 'My Account',
+        'bn' : 'আমার একাউন্ট'
+    },
+    {
+        'key': 'Wishlist',
+        'en' : 'Wishlist',
+        'bn' : 'উইশলিস্ট'
+    },
+    {
+        'key': 'Cart',
+        'en' : 'Cart',
+        'bn' : 'কার্ট'
+    },
+    {
+        'key': 'Sorry, No Product found by',
+        'en' : 'Sorry, No Product found by',
+        'bn' : 'দু:খিত, কোন প্রোডাক্ট পাওয়া যায়নি'
+    },
+    {
+        'key': 'Please try with another keyword',
+        'en' : 'Please try with another keyword',
+        'bn' : 'অন্য কীওয়ার্ড দিয়ে চেষ্টা করুন'
+    },
+
+
+
+
+
+    // Category Translations, Do this after the above translations
+    // @todo Add More Categories Translation
+    {
+        'key': 'Electronics',
+        'en' : 'Electronics',
+        'bn' : 'ইলেকট্রনিক্স'
+    },
+    {
+        'key': 'Clothing & Accessories',
+        'en' : 'Clothing & Accessories',
+        'bn' : 'পোশাক ও আনুষাঙ্গিক'
+    },
+    {
+        'key': 'Groceries',
+        'en' : 'Groceries',
+        'bn' : 'মুদির জিনিসপত্র'
+    },
+    {
+        'key': 'Health',
+        'en' : 'Health',
+        'bn' : 'স্বাস্থ্য'
+    },
+    {
+        'key': 'Beauty & Personal Care',
+        'en' : 'Beauty & Personal Care',
+        'bn' : 'সৌন্দর্য ও ব্যক্তিগত'
+    },
+    {
+        'key': 'Toys & Games',
+        'en' : 'Toys & Games',
+        'bn' : 'খেলনা ও গেমস'
+    },
+    {
+        'key': 'Pharmacy',
+        'en' : 'Pharmacy',
+        'bn' : 'ফার্মেসী'
+    },
+    {
+        'key': 'Food',
+        'en' : 'Food',
+        'bn' : 'খাদ্য'
+    },
+
+    // Sub Categories
+    {
+        'key': 'Laptop',
+        'en' : 'Laptop',
+        'bn' : 'ল্যাপটপ'
+    },
+    {
+        'key': 'SmartPhone',
+        'en' : 'SmartPhone',
+        'bn' : 'স্মার্টফোন'
+    },
+    {
+        'key': 'Camera',
+        'en' : 'Camera',
+        'bn' : 'ক্যামেরা'
+    },
+    {
+        'key': 'TV',
+        'en' : 'TV',
+        'bn' : 'টেলিভিশন'
+    },
+];
