2382570dd3bb9e656570e3818ac684248586847e
ma-foyez <mafoyez.bd@gmail.com>
2021-08-07 23:00:52 +0600

    Footer page section

--- pages/p/[pageBySlug].js
+++ pages/p/[pageBySlug].js
@@ -3,7 +3,8 @@ import { useRouter } from 'next/router';
 import { useSelector, useDispatch } from "react-redux";
 import MainLayout from "../../components/layouts/MainLayout";
 import { getWebsitePageAction } from "../../components/_redux/WebsiteInformation/Action/WebsiteInformationAction";
-import ReactHtmlParser  from 'react-html-parser';
+import ReactHtmlParser from 'react-html-parser';
+import LoadingSpinner from "../../components/master/LoadingSpinner/LoadingSpinner";
 
 export default function PageBySlug() {
 
@@ -20,15 +21,21 @@ export default function PageBySlug() {
         return splitStr.join(' ');
     }
 
-    const {pageData} = useSelector((state) => state.WebsitePageReducer);
+    const { pageData, isLoading } = useSelector((state) => state.WebsitePageReducer);
 
     useEffect(() => {
-        dispatch(getWebsitePageAction( pageBySlug ));
+        dispatch(getWebsitePageAction(pageBySlug));
     }, [router.query]);
 
     return (
         <MainLayout pageTitle={typeof pageData !== 'undefined' && pageData !== null ? pageData.title : ''}>
+
             <div className="row m-3 justify-content-center">
+                {
+                    isLoading && <LoadingSpinner text="Page Loading....." />
+
+                }
+                
                 {
                     typeof pageData !== 'undefined' && pageData !== null &&
                     <div className="col-lg-9">
@@ -43,6 +50,22 @@ export default function PageBySlug() {
                             </div>
                         </div>
                     </div>
+                }
+                {
+                    !isLoading && (typeof pageData === "undefined" || pageData === null) && (
+                        <div className="col-lg-9">
+                            <div className="card shadow rounded">
+                                <div className="card-header">
+                                    <h6>{pageTitle}</h6>
+                                </div>
+                                <div className="card-body">
+                                    <div>
+                                        {pageTitle} page coming soon....
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
                 }
             </div>
         </MainLayout>
