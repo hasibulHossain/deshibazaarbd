d50f98a8a470fe0ceccfcecdf5366154021ac85b
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-08-12 12:41:40 +0600

    Fixed Header Categories

--- components/Header/_redux/HeaderAction/HeaderAction.js
+++ components/Header/_redux/HeaderAction/HeaderAction.js
@@ -1,7 +1,7 @@
 import Axios from "axios";
 import * as Types from "../Type/Types";
 
-const url = `${process.env.NEXT_PUBLIC_API_URL}frontend-categories`;
+const url = `${process.env.NEXT_PUBLIC_API_URL}frontend-categories?type=navbar`;
 
 /**
  * Get Menu List
@@ -15,13 +15,8 @@ const url = `${process.env.NEXT_PUBLIC_API_URL}frontend-categories`;
 export const getMenuListData = () => async (dispatch) => {
   try {
     dispatch({ type: Types.INIT_MENU_LIST, payload: { isLoading: true } });
-    const res      = await Axios.get(url);
-    const menuList = res.data.data;
-
-    // filter menu list and remove unnecessary menu items
-    const filteredMenuList = menuList.filter( menu => menu.short_code !== 'others' )
-
-    dispatch({ type: Types.GET_MENU_LIST, payload: { menuList: filteredMenuList } });
+    const res = await Axios.get(url);
+    dispatch({ type: Types.GET_MENU_LIST, payload: { menuList: res.data.data } });
   } catch (error) {
     dispatch({ type: Types.FETCH_MENU_LIST_FAILED, payload: { error: true } });
   }
