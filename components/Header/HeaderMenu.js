4ef78928719b8a1bd654f8765b6840292a1ee4f5
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-08-15 15:44:11 +0600

    Fixed multiple api call for header menu

--- components/Header/HeaderMenu.js
+++ components/Header/HeaderMenu.js
@@ -21,7 +21,9 @@ const HeaderMenu = ({ toggleNav }) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getMenuListData());
+    if (!menuList.length) {
+      dispatch(getMenuListData());
+    }
   }, []);
 
   /**
