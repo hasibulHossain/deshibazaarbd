3b683881d9927664c65f59becbc3058b8abbb7d6
Maniruzzaman Akash <manirujjamanakash@gmail.com>
2021-08-15 11:12:45 +0600

    Fixed View all button design and fixed some title in company policy

--- components/CompanyPolicy/_redux/Action/CompanyPolicyAction.js
+++ components/CompanyPolicy/_redux/Action/CompanyPolicyAction.js
@@ -12,17 +12,17 @@ export const getCompanyPolicyList = () => (dispatch) => {
     const data = [
         {
             title: "30 days return",
-            description: "no question ask",
+            description: "No Question Ask",
             icon: "/images/policies/30-day-return.png"
         },
         {
             title: "secure payment",
-            description: "100% secure payment",
+            description: "100% Secure Payment",
             icon: "/images/policies/secure-payment.png"
         },
         {
             title: "24/7 support",
-            description: "dedicated support",
+            description: "Dedicated Support",
             icon: "/images/policies/24-7-support.png"
         },
      
