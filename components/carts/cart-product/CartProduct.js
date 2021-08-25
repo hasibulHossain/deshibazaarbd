594c58df31b876b222ccb62f2f5210b56b853d31
ma-foyez <mafoyez.bd@gmail.com>
2021-08-26 00:25:31 +0600

    cart page responsive issue fixed

--- components/carts/cart-product/CartProduct.js
+++ components/carts/cart-product/CartProduct.js
@@ -34,54 +34,47 @@ const CartProduct = ({ cart }) => {
             <FontAwesomeIcon icon={faTimes} />
           </button>
           <p className="remove_title">
-            Remove from cart 
+            Remove from cart
           </p>
           <div className="mb-3">
             Product will be removed from your Cart...
           </div>
           <div className="d-flex justify-content-end">
-              <button className="custom_secondary_btn custom-button-component" onClick={handleClose}>Cancel</button>
-              <button className="custom-button-component ml-3" style={{ padding: '5px 10px' }} onClick={() => handleDeleteCartProduct(cart.productID)}>Remove</button>
+            <button className="custom_secondary_btn custom-button-component" onClick={handleClose}>Cancel</button>
+            <button className="custom-button-component ml-3" style={{ padding: '5px 10px' }} onClick={() => handleDeleteCartProduct(cart.productID)}>Remove</button>
           </div>
         </Modal.Body>
       </Modal>
 
       <div className="row justify-content-between">
-        <div className="col-md-7">
-          <div className="product_cart_inner row">
-            <div className="col-4 product_cart_left">
-              <input type="checkbox" className="cart-checkbox" checked={cart.isChecked} onChange={() => dispatch(toggleAllCartSelection(! cart.isChecked, cart.productID))} />
-              <img
-                src={cart.productImage === null ? 'https://img.icons8.com/plasticine/2x/image.png' : cart.productImage }
-                alt={cart.productName}
-                className="img-thumbnail product_cart_img"
-              />
+        <div className="col-md-3 product_cart_left">
+          <input type="checkbox" className="cart-checkbox pointer mr-2" checked={cart.isChecked} onChange={() => dispatch(toggleAllCartSelection(!cart.isChecked, cart.productID))} />
+          <img
+            src={cart.productImage === null ? 'https://img.icons8.com/plasticine/2x/image.png' : cart.productImage}
+            alt={cart.productName}
+            className="img-thumbnail product_cart_img"
+          />
+        </div>
+        <div className="col-md-5">
+          <div className="product_cart_inner_details mt-2">
+            <h4 className="innser_cart_product_name pointer" onClick={() => dispatch(toggleProductModalAction(cart.sku))}>{cart.productName}</h4>
+
+            <div className="cart_product_price">
+              <span className="price">
+                {formatCurrency(cart.offerPrice !== null && cart.offerPrice !== "0" ? cart.offerPrice : cart.price)}
+              </span>
+
+              <span className="offer_price">
+                {formatCurrency(cart.offerPrice !== null && cart.offerPrice !== "0" ? cart.price : 0)}
+              </span>
             </div>
-            <div className="product_cart_inner_details col-8">
-              <h4 className="innser_cart_product_name pointer" onClick={() => dispatch(toggleProductModalAction(cart.sku))}>{cart.productName}</h4>
 
-              {/* <div className="cart_product_info">
-                <span>No Brand, </span>
-                <span>Ring Size:Adjustable, </span>
-                <span>Color Family:Silver</span>
-              </div> */}
-
-              <div className="cart_product_price">
-                <span className="price">
-                  { formatCurrency(cart.offerPrice !== null && cart.offerPrice !== "0" ? cart.offerPrice : cart.price) }
-                </span>
-
-                <span className="offer_price">
-                  { formatCurrency(cart.offerPrice !== null && cart.offerPrice !== "0" ? cart.price : 0) }
-                </span>
-              </div>
-
-              <p className="discount_percantage">
-                { (((cart.price - cart.offerPrice) * 100) / cart.price).toFixed(0) }%
-              </p>
-            </div>
+            <p className="discount_percantage">
+              {(((cart.price - cart.offerPrice) * 100) / cart.price).toFixed(0)}%
+            </p>
           </div>
         </div>
+    
         <div className="col-md-4">
           <div className="product_cart_purchase">
             <CartQuantity cart={cart} />
@@ -90,7 +83,7 @@ const CartProduct = ({ cart }) => {
           </div>
         </div>
       </div>
-    
+
     </>
   );
 };
