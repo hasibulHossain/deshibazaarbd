c55365bd4df4d67da80d24e3ee312dc022d29791
ma-foyez <mafoyez.bd@gmail.com>
2021-08-27 01:03:07 +0600

    my accout page responsive issue fixed

--- components/myprofile/ProductProfile.js
+++ components/myprofile/ProductProfile.js
@@ -30,145 +30,141 @@ const ProductProfile = () => {
 
   const toggleShowHandler = () => {
     setShow(preState => !preState);
-  } 
+  }
 
   return (
     <>
       <div className="wishbanner pb">
         <div className="container">
           <div className="row">
-            <div className="col-md-3">
+            <div className="col-lg-3 col-md-5">
               <ProfileSideBar />
             </div>
 
             {/** user profile data */}
-            <div className="col-md-9">
-              <div className="row mt-5">
-                <div className="col-md-6">
-                  <div className="card mb-2 default_height shadow-sm p-3 mb-5 bg-white rounded">
-                    <h6> <Translate>Personal Profile</Translate> <span className="edit_profile_link">
-                      <Link href="/account-setting#personal-info-edit">
-                        <a className="text-decoration-none">
-                          <span className="edit_profile_link ml-2"><Translate>EDIT</Translate></span>
-                        </a>
-                      </Link>
-                    </span></h6>
-                    <div className="border-top">
-                      <div className="text-center mt-2">
-                        {/* <div className="border rounded-circle p-1" style={{height: "90px", width: "90px"}}>
-                          <FontAwesomeIcon icon={faUser} style={{ fontSize: "70px" }} />
-                        </div> */}
-                        <img className="border rounded-circle p-1 mb-2" style={{ height: "100px" }} src="https://cdn.iconscout.com/icon/free/png-256/laptop-user-1-1179329.png" alt="user image" />
-                      </div>
-                      <p className="user_name text-capitalize"> {`${userData !== null && userData.first_name} ${(userData !== null && userData.last_name !== null) ? userData.last_name : ''}`}</p>
-                      <p>
-                        <span className="user_icon">
-                          <FontAwesomeIcon icon={faMailBulk} />
-                        </span>
-                        <span className="user_email">
-                          {userData !== null && userData.email}
-                        </span>
-                      </p>
-                      <p>
-                        <span className="user_icon">
-                          <FontAwesomeIcon icon={faPhone} />
-                        </span>
-                        <span className="user_phone">
-                          {userData !== null && userData.phone_no}
-                        </span>
-                      </p>
+            <div className="col-lg-9 col-md-7">
+              <div className="row mt-4">
+                <div className="col-lg-6 card default_height shadow-sm p-3 m-1 bg-white rounded">
+                  <h6> <Translate>Personal Profile</Translate> <span className="edit_profile_link">
+                    <Link href="/account-setting#personal-info-edit">
+                      <a className="text-decoration-none">
+                        <span className="edit_profile_link ml-2"><Translate>EDIT</Translate></span>
+                      </a>
+                    </Link>
+                  </span></h6>
+                  <div className="border-top">
+                    <div className="text-center mt-2">
+
+                      <img className="border rounded-circle p-1 mb-2" style={{ height: "100px" }} src="https://cdn.iconscout.com/icon/free/png-256/laptop-user-1-1179329.png" alt="user image" />
                     </div>
+                    <p className="user_name text-capitalize"> {`${userData !== null && userData.first_name} ${(userData !== null && userData.last_name !== null) ? userData.last_name : ''}`}</p>
+                    <p>
+                      <span className="user_icon">
+                        <FontAwesomeIcon icon={faMailBulk} />
+                      </span>
+                      <span className="user_email">
+                        {userData !== null && userData.email}
+                      </span>
+                    </p>
+                    <p>
+                      <span className="user_icon">
+                        <FontAwesomeIcon icon={faPhone} />
+                      </span>
+                      <span className="user_phone">
+                        {userData !== null && userData.phone_no}
+                      </span>
+                    </p>
                   </div>
+                 
                 </div>
-                <div className="col-md-5">
-                  <div className="card mb-2 p-3 default_height">
-                    <div className="card-title">
-                      <h6> <Translate>Address Book</Translate> 
+                <div className="col-lg-5 card m-1 p-3 default_height">
+                  <div className="card-title">
+                    <h6> <Translate>Address Book</Translate>
 
-                        {
-                          ((defaultBillingAddress.length > 0) || (defaultShippingAddress.length > 0)) && (
-                            <Link href="/account-setting#address-book">
-                              <a className="text-decoration-none">
-                                <span className="edit_profile_link ml-2"><Translate>EDIT</Translate></span>
-                              </a>
-                            </Link>
-                          )
-                        }
+                      {
+                        ((defaultBillingAddress.length > 0) || (defaultShippingAddress.length > 0)) && (
+                          <Link href="/account-setting#address-book">
+                            <a className="text-decoration-none">
+                              <span className="edit_profile_link ml-2"><Translate>EDIT</Translate></span>
+                            </a>
+                          </Link>
+                        )
+                      }
 
-                        {/* <Link href="/account-setting#address-book">
-                          <a className="text-decoration-none">
-                            <span className="edit_profile_link ml-2">EDIT</span>
-                          </a>
-                        </Link> */}
+                      {/* <Link href="/account-setting#address-book">
+                        <a className="text-decoration-none">
+                          <span className="edit_profile_link ml-2">EDIT</span>
+                        </a>
+                      </Link> */}
 
-                        {
-                          !isLoading && defaultBillingAddress && defaultBillingAddress.length === 0 && defaultShippingAddress.length === 0 && (
-                            <span className="edit_profile_link ml-2" onClick={toggleShowHandler}>ADD NEW</span>
-                          )
-                        }
+                      {
+                        !isLoading && defaultBillingAddress && defaultBillingAddress.length === 0 && defaultShippingAddress.length === 0 && (
+                          <span className="edit_profile_link ml-2" onClick={toggleShowHandler}>ADD NEW</span>
+                        )
+                      }
 
-                      </h6>
-                      <div className="border-top">
-                        <p className="address_sub_title mt-2">
-                          Default Shipping Address :
-                        </p>
-                        {
-                          isLoading && (
-                            <LoadingSpinner text="Loading Shipping Address..." />
-                          )
-                        }
-                        {
-                          defaultShippingAddress && defaultShippingAddress.length > 0 && (
-                            <>
-                              <p>
-                                <span className="user_icon">
-                                  <FontAwesomeIcon icon={faMapMarkedAlt} />
-                                </span>
-                                <span className="user_address">
-                                  {defaultShippingAddress[0].area}, {defaultShippingAddress[0].street1}, {defaultShippingAddress[0].city} <br />
-                                  {defaultShippingAddress[0].country}
-                                </span>
-                              </p>
-                            </>
-                          )
-                        }
-                        {
-                          !isLoading && defaultShippingAddress && defaultShippingAddress.length === 0 && (
-                            <WarningMessage text="Default shipping address not found" />
-                          )
-                        }
-                        <p className="address_sub_title">
-                          Default Billing Address :
-                        </p>
-                        {
-                          isLoading && (
-                            <LoadingSpinner text="Loading Billing Address..." />
-                          )
-                        }
-                        {
-                          defaultBillingAddress && defaultBillingAddress.length > 0 && (
-                            <>
-                              <p>
-                                <span className="user_icon">
-                                  <FontAwesomeIcon icon={faMapMarkedAlt} />
-                                </span>
-                                <span className="user_address">
-                                  {defaultBillingAddress[0].area}, {defaultBillingAddress[0].street1}, {defaultBillingAddress[0].city} <br />
-                                  {defaultBillingAddress[0].country}
-                                </span>
-                              </p>
-                            </>
-                          )
-                        }
-                        {
-                          !isLoading && defaultBillingAddress && defaultBillingAddress.length === 0 && (
-                            <WarningMessage text="Default billing address not found" />
-                          )
-                        }
-                      </div>
-
+                    </h6>
+                    <div className="border-top">
+                      <p className="address_sub_title mt-2">
+                        Default Shipping Address :
+                      </p>
+                      {
+                        isLoading && (
+                          <LoadingSpinner text="Loading Shipping Address..." />
+                        )
+                      }
+                      {
+                        defaultShippingAddress && defaultShippingAddress.length > 0 && (
+                          <>
+                            <p>
+                              <span className="user_icon">
+                                <FontAwesomeIcon icon={faMapMarkedAlt} />
+                              </span>
+                              <span className="user_address">
+                                {defaultShippingAddress[0].area}, {defaultShippingAddress[0].street1}, {defaultShippingAddress[0].city} <br />
+                                {defaultShippingAddress[0].country}
+                              </span>
+                            </p>
+                          </>
+                        )
+                      }
+                      {
+                        !isLoading && defaultShippingAddress && defaultShippingAddress.length === 0 && (
+                          <WarningMessage text="Default shipping address not found" />
+                        )
+                      }
+                      <p className="address_sub_title">
+                        Default Billing Address :
+                      </p>
+                      {
+                        isLoading && (
+                          <LoadingSpinner text="Loading Billing Address..." />
+                        )
+                      }
+                      {
+                        defaultBillingAddress && defaultBillingAddress.length > 0 && (
+                          <>
+                            <p>
+                              <span className="user_icon">
+                                <FontAwesomeIcon icon={faMapMarkedAlt} />
+                              </span>
+                              <span className="user_address">
+                                {defaultBillingAddress[0].area}, {defaultBillingAddress[0].street1}, {defaultBillingAddress[0].city} <br />
+                                {defaultBillingAddress[0].country}
+                              </span>
+                            </p>
+                          </>
+                        )
+                      }
+                      {
+                        !isLoading && defaultBillingAddress && defaultBillingAddress.length === 0 && (
+                          <WarningMessage text="Default billing address not found" />
+                        )
+                      }
                     </div>
+
                   </div>
+                  
                 </div>
               </div>
             </div>
