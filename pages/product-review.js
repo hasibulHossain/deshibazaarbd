614d6a7608d989cc60371176b47e3a6d850dc508
ma-foyez <mafoyez.bd@gmail.com>
2021-08-29 14:09:16 +0600

    Private route set

--- pages/product-review.js
+++ pages/product-review.js
@@ -1,10 +1,11 @@
 import React from "react";
 import MainLayout from "../components/layouts/MainLayout";
+import ProtectedRoute from "../components/master/protectedRoute/ProtectedRoute";
 import ProductReview from '../components/product-review/ProductReview';
 
 function productReview() {
     return (
-        <MainLayout pageTitle="Deshi Bazaar BD || Product Revew">
+        <MainLayout pageTitle="Product Review">
             <div className="container">
                 <ProductReview />
             </div>
@@ -12,4 +13,4 @@ function productReview() {
     );
 }
 
-export default productReview;
+export default ProtectedRoute(productReview);
