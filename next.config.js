b100978512199586bfbf3915c626f714328c4216
hasibulhossain52@gmail.com <hasibulhossain52@gmail.com>
2021-09-13 15:26:25 +0600

    updated version to 0.4.6

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-})
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//   enabled: process.env.ANALYZE === 'true',
+// })
 
-module.exports = withBundleAnalyzer({
+// module.exports = withBundleAnalyzer({
+//   images: {
+//     domains: ['deshi.programmingshikhi.com', 'api-ecom.programmingshikhi.com'],
+//   },
+//   webpack5: true
+// })
+
+  
+module.exports = {
   images: {
     domains: ['deshi.programmingshikhi.com', 'api-ecom.programmingshikhi.com'],
   },
-})
+}
