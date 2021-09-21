cb04b27ebfd2eb52153a04e48b40c71b10d1d5bd
hasibulhossain52@gmail.com <hasibulhossain52@gmail.com>
2021-09-21 15:55:03 +0600

    warp product div with a tag for new tab support

--- components/products/ProductSingleMini.js
+++ components/products/ProductSingleMini.js
@@ -6,12 +6,13 @@ import { faShoppingBag } from "@fortawesome/free-solid-svg-icons";
 
 import PriceCalculation from "./partials/PriceCalculation";
 import { useDispatch } from "react-redux";
-import { toggleProductModalAction } from "./_redux/Action/ProductAction";
+// import { toggleProductModalAction } from "./_redux/Action/ProductAction";
 import { addToCartAction } from "../carts/_redux/action/CartAction";
 import { showToast } from "../master/Helper/ToastHelper";
 import Translate from "../translation/Translate";
 import router from 'next/router'
 import Image from 'next/image';
+import Link from 'next/link';
 
 /**
  * ProductSingleMini Component
@@ -87,8 +88,135 @@ const ProductSingleMini = ({
         <div className={`product-card-body`} >
           {columnClassName === "col-md-2" && (
             <>
-              <div onClick={() => redirectToProductDetailsPage(item)} style={{overflow: 'hidden'}}>
-              <Image src={imageURL} alt={item.name} width={200} height={200} />
+              <Link href={`/products/${item.sku}`}>
+                <a className="product-card-link">
+                  <div style={{overflow: 'hidden'}}>
+                  <Image src={imageURL} alt={item.name} width={200} height={200} />
+                    <div className="product-card-body-inner">
+                      <p className="product-title">
+                        <Translate>{item.name}</Translate>
+                      </p>
+                      <span className="product-unit">{item.per_unit_value} {' '} {item.unit_name && item.unit_name}</span>
+                      <p
+                        className={`stock-status ${
+                          parseInt(item.current_stock) > 0
+                            ? "stock-status-in"
+                            : "stock-status-out"
+                        }`}
+                      >
+                        <span>
+                          {parseInt(item.current_stock) > 0
+                            ? "In stock"
+                            : "Out of stock"}
+                        </span>
+                      </p>
+                      <PriceCalculation item={item} />
+                      <div
+                        className={
+                          columnClassName === "col-md-3" || "col-md-2"
+                            ? ""
+                            : "d-flex justify-content-start"
+                        }
+                      >
+                        {/* <ProductRating rating={item.average_rating} /> */}
+                        {/* {
+                                                  item.average_rating != 0 && (
+                                                      <ProductRating rating={item.average_rating} />
+                                                  )
+                                              } */}
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </Link>
+              <div className="product-single-mini-cart">
+                <button type='button' onClick={() => addToCart(item)} className='simple-btn homepage-product-btn'>
+                  <div className="simple-btn__inner">
+                    <div className="simple-btn__icon">
+                      <FontAwesomeIcon icon={faShoppingBag} />
+                    </div>
+                    <span className="simple-btn__txt">Add to cart</span>
+                  </div>
+                </button>
+              </div>
+            </>
+          )}
+
+          {(columnClassName === "col-md-3" ||
+            columnClassName === "col-md-12") && (
+            <>
+              <div className="row">
+                <div
+                  className={
+                    columnClassName === "col-md-3" ? "col-md-12" : "col-7"
+                  }
+                  > 
+                  <Link href={`/products/${item.sku}`}>
+                    <a className="product-card-link">
+                  <div style={{overflow: 'hidden'}}>
+                    <img src={imageURL} alt={item.name} className="img-fluid" />
+                  </div>
+                  <div>
+                    <p className="product-title">{item.name}</p>
+                    <span className="product-unit">{item.per_unit_value} {' '} {item.unit_name && item.unit_name}</span>
+                    <p
+                      className={`stock-status ${
+                        parseInt(item.current_stock) > 0
+                        ? "stock-status-in"
+                        : "stock-status-out"
+                      }`}
+                      >
+                      <span>
+                        {parseInt(item.current_stock) > 0
+                          ? "In stock"
+                          : "Out of stock"}
+                      </span>
+                    </p>
+                    <PriceCalculation item={item} />
+                    {/* <div
+                      className={
+                        columnClassName === "col-md-3"
+                        ? ""
+                        : "d-flex justify-content-start"
+                      }
+                    > */}
+                      {/* {
+                        item.average_rating != 0 && (
+                          <ProductRating rating={item.average_rating} />
+                          )
+                        } */}
+                    {/* </div> */}
+                  </div>
+                      </a>
+                  </Link>
+                </div>
+              </div>
+              <div className="product-single-mini-cart">
+              <button type='button' onClick={() => addToCart(item)} className='simple-btn product-btn'>
+                <div className="simple-btn__inner">
+                  <div className="simple-btn__icon">
+                    <FontAwesomeIcon icon={faShoppingBag} />
+                  </div>
+                  <span className="simple-btn__txt">Add to cart</span>
+                </div>
+              </button>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+
+  if(isSliding) {
+    singleProduct = (
+      <div className="product-card filter_column_3">
+      <div className="product-card-body">
+        <Link href={`/products/${item.sku}`}>
+          <a className="product-card-link">
+            <div style={{overflow: 'hidden'}}>
+            <Image src={imageURL} alt={item.name} width={200} height={200} />
+            
               <div className="product-card-body-inner">
                 <p className="product-title">
                   <Translate>{item.name}</Translate>
@@ -115,129 +243,11 @@ const ProductSingleMini = ({
                       : "d-flex justify-content-start"
                   }
                 >
-                  {/* <ProductRating rating={item.average_rating} /> */}
-                  {/* {
-                                            item.average_rating != 0 && (
-                                                <ProductRating rating={item.average_rating} />
-                                            )
-                                        } */}
                 </div>
               </div>
-              </div>
-              <div className="product-single-mini-cart">
-                <button type='button' onClick={() => addToCart(item)} className='simple-btn homepage-product-btn'>
-                  <div className="simple-btn__inner">
-                    <div className="simple-btn__icon">
-                      <FontAwesomeIcon icon={faShoppingBag} />
-                    </div>
-                    <span className="simple-btn__txt">Add to cart</span>
-                  </div>
-                </button>
-              </div>
-            </>
-          )}
-
-          {(columnClassName === "col-md-3" ||
-            columnClassName === "col-md-12") && (
-            <>
-              <div className="row">
-                {/* <div
-                  className={ columnClassName == "col-md-3" ? "col-md-12" : "col-5" } >
-                </div> */}
-                <div
-                  onClick={() => redirectToProductDetailsPage(item)}
-                  className={
-                    columnClassName === "col-md-3" ? "col-md-12" : "col-7"
-                  }
-                > 
-                  <div style={{overflow: 'hidden'}}>
-                    <img src={imageURL} alt={item.name} className="img-fluid" />
-                  </div>
-                  <div>
-                    <p className="product-title">{item.name}</p>
-                    <span className="product-unit">{item.per_unit_value} {' '} {item.unit_name && item.unit_name}</span>
-                    <p
-                      className={`stock-status ${
-                        parseInt(item.current_stock) > 0
-                          ? "stock-status-in"
-                          : "stock-status-out"
-                      }`}
-                    >
-                      <span>
-                        {parseInt(item.current_stock) > 0
-                          ? "In stock"
-                          : "Out of stock"}
-                      </span>
-                    </p>
-                    <PriceCalculation item={item} />
-                    {/* <div
-                      className={
-                        columnClassName === "col-md-3"
-                          ? ""
-                          : "d-flex justify-content-start"
-                      }
-                    > */}
-                      {/* {
-                                                item.average_rating != 0 && (
-                                                    <ProductRating rating={item.average_rating} />
-                                                )
-                                            } */}
-                    {/* </div> */}
-                  </div>
-                </div>
-              </div>
-              <div className="product-single-mini-cart">
-              <button type='button' onClick={() => addToCart(item)} className='simple-btn product-btn'>
-                <div className="simple-btn__inner">
-                  <div className="simple-btn__icon">
-                    <FontAwesomeIcon icon={faShoppingBag} />
-                  </div>
-                  <span className="simple-btn__txt">Add to cart</span>
-                </div>
-              </button>
-              </div>
-            </>
-          )}
-        </div>
-      </div>
-    </div>
-  )
-
-  if(isSliding) {
-    singleProduct = (
-      <div className="product-card filter_column_3">
-      <div className="product-card-body">
-        <div onClick={() => redirectToProductDetailsPage(item)} style={{overflow: 'hidden'}}>
-        <Image src={imageURL} alt={item.name} width={200} height={200} />
-        <div className="product-card-body-inner">
-          <p className="product-title">
-            <Translate>{item.name}</Translate>
-          </p>
-          <span className="product-unit">{item.per_unit_value} {' '} {item.unit_name && item.unit_name}</span>
-          <p
-            className={`stock-status ${
-              parseInt(item.current_stock) > 0
-                ? "stock-status-in"
-                : "stock-status-out"
-            }`}
-          >
-            <span>
-              {parseInt(item.current_stock) > 0
-                ? "In stock"
-                : "Out of stock"}
-            </span>
-          </p>
-          <PriceCalculation item={item} />
-          <div
-            className={
-              columnClassName === "col-md-3" || "col-md-2"
-                ? ""
-                : "d-flex justify-content-start"
-            }
-          >
-          </div>
-        </div>
-        </div>
+            </div>
+          </a>
+        </Link>
         <div className="product-single-mini-cart">
           <button type='button' onClick={() => addToCart(item)} className='simple-btn homepage-product-btn'>
             <div className="simple-btn__inner">
