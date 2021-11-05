04d67279119284c931d08558e6b40744a9f2ed7d
hasibulhossain52@gmail.com <hasibulhossain52@gmail.com>
2021-11-05 15:55:20 +0600

    added spinner on withProtedRoute

--- components/master/hoc/withProtectedRoute.js
+++ components/master/hoc/withProtectedRoute.js
@@ -29,7 +29,9 @@ const withProtectedRoute = (WrappedComponent, isSignedIn = false) => {
         if(!session && loading) {
             return (
                 <div className="text-center py-5">
-                    Loading...
+                    <div class="spinner-border text-secondary" style={{width: '50px', height: '50px', fontSize: '15px'}} role="status">
+                        <span class="sr-only">Loading...</span>
+                    </div>
                 </div>
             )
         }
