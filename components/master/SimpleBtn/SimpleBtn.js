70869576ac74df082d035465cfdb1ffbe36977a7
hasibulhossain52@gmail.com <hasibulhossain52@gmail.com>
2021-11-06 15:00:40 +0600

    added disabled props on simplebtn

--- components/master/SimpleBtn/SimpleBtn.js
+++ components/master/SimpleBtn/SimpleBtn.js
@@ -5,9 +5,9 @@ import React from "react";
 // danger
 
 function SimpleBtn(props) {
-  const { children, variant, style = {}, onClick, type } = props;
+  const { children, variant, style = {}, onClick, type, disabled=false } = props;
   return (
-    <button type={type ? type : 'button'} onClick={onClick && onClick} className={`simple-btn ${variant}`} style={{ ...style }}>
+    <button type={type ? type : 'button'} disabled={disabled} onClick={onClick && onClick} className={`simple-btn ${variant}`} style={{ ...style }}>
       {children}
     </button>
   );
