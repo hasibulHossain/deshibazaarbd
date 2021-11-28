ae0b3e3c503cef5b2d8c71fa704ce49d6c42fb11
hasibulhossain52@gmail.com <hasibulhossain52@gmail.com>
2021-11-28 17:38:06 +0600

    added facebook pixel

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import App from "next/app";
 import Head from 'next/head';
 import { Provider } from "react-redux";
-import Router from "next/router";
+import Router, { useRouter } from "next/router";
 import NProgress from 'nprogress';
 import { createWrapper } from "next-redux-wrapper";
+import Script from 'next/script'
 import Store from "../_redux/Store";
+import * as fbq from '../lib/fpixel'
 
 import "../assets/scss/variables.scss";
 import "bootstrap/scss/bootstrap.scss";
@@ -57,10 +59,43 @@ Router.onRouteChangeError = () => {
 };
 
 
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    // This pageview only triggers the first time (it's important for Pixel to have real information)
+    fbq.pageview()
+
+    const handleRouteChange = () => {
+      fbq.pageview()
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return (
+    <>
+      {/* Global Site Code Pixel - Facebook Pixel */}
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', ${fbq.FB_PIXEL_ID});
+          `,
+        }}
+      />
       <Provider store={Store}>
         <Head>
           <link rel="shortcut icon" href="/favicon.ico" />
@@ -71,8 +106,8 @@ class MyApp extends App {
             <Component {...pageProps}></Component>
           </MainLayout>
       </Provider>
-    );
-  }
+    </>
+  )
 }
 
 const makeStore = () => Store;
--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-
+import { FB_PIXEL_ID } from '../lib/fpixel'
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
@@ -11,6 +11,14 @@ class MyDocument extends Document {
       <Html lang="en-US">
         <Head>
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600&display=swap" />
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: 'none' }}
+              src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+            />
+          </noscript>
         </Head>
         <body>
           <Main />
