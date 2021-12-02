f1ac94e916d4b0fc5e8a30888179839e37b4b79a
hasibulhossain52@gmail.com <hasibulhossain52@gmail.com>
2021-12-02 17:30:44 +0600

    added facebook pixel package

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,10 @@
 import React, { useEffect } from "react";
-import App from "next/app";
 import Head from 'next/head';
 import { Provider } from "react-redux";
 import Router, { useRouter } from "next/router";
 import NProgress from 'nprogress';
 import { createWrapper } from "next-redux-wrapper";
-import Script from 'next/script'
 import Store from "../_redux/Store";
-import * as fbq from '../lib/fpixel'
 
 import "../assets/scss/variables.scss";
 import "bootstrap/scss/bootstrap.scss";
@@ -64,39 +61,22 @@ function MyApp({ Component, pageProps }) {
   const router = useRouter()
 
   useEffect(() => {
-    // This pageview only triggers the first time (it's important for Pixel to have real information)
-    fbq.pageview()
+    import('react-facebook-pixel')
+    .then((x) => x.default)
+    .then((ReactPixel) => {
+      ReactPixel.init(process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID) // facebookPixelId
+      ReactPixel.pageView()
 
-    const handleRouteChange = () => {
-      fbq.pageview()
-    }
-
-    router.events.on('routeChangeComplete', handleRouteChange)
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
+      router.events.on('routeChangeComplete', () => {
+        ReactPixel.pageView()
+      })
+    })
   }, [router.events])
 
   return (
     <>
       <Provider store={Store}>
         {/* Global Site Code Pixel - Facebook Pixel */}
-        <Script
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', ${fbq.FB_PIXEL_ID});
-            `,
-          }}
-        />
         <Head>
           <link rel="shortcut icon" href="/favicon.ico" />
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
--- pages/_document.js
+++ pages/_document.js
@@ -11,14 +11,6 @@ class MyDocument extends Document {
       <Html lang="en-US">
         <Head>
           <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600&display=swap" />
-          <noscript>
-            <img
-              height="1"
-              width="1"
-              style={{ display: 'none' }}
-              src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
-            />
-          </noscript>
         </Head>
         <body>
           <Main />
