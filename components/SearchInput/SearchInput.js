5c2f7f45d28bcdefdc0de07e2eb98dd4544887e3
hasibulhossain52@gmail.com <hasibulhossain52@gmail.com>
2021-12-07 19:18:12 +0600

    added search history

--- components/SearchInput/SearchInput.js
+++ components/SearchInput/SearchInput.js
@@ -8,15 +8,39 @@ import { formatCurrency } from "../../services/currency";
 import axios from "axios";
 import { toggleBackdrop } from "../../_redux/store/action/globalAction";
 
+const ISSERVER = typeof window === "undefined";
+
+const data = [
+  {
+    id  : 12341324123,
+    name: 'mobile'
+  },
+  {
+    id  : 12341323123,
+    name: 'samsung'
+  },
+  {
+    id  : 12341324123,
+    name: 't-shirt'
+  },
+]
+
+
 const SearchInput = () => {
   const dispatch = useDispatch();
   const router = useRouter();
   const [search, setSearch] = useState("");
+  const [isSearched, setIsSearched] = useState(false);
+  const [isSuggestionVisible, setIsSuggestionVisible] = useState(false);
   const [searchType, setSearchType] = useState("product"); // products || shops || brands
+  const [searchHistory, setSearchHistory] = useState([]);
+
   const suggestions = useSelector((state) => state.SearchReducer.products);
   const loading = useSelector((state) => state.SearchReducer.loading);
   const firstRenderRef = useRef(true);
   const searchRef = useRef();
+
+
 
   const searchByList = [
     {label: 'products', id: 'product'},
@@ -26,14 +50,28 @@ const SearchInput = () => {
   ];
 
   const searchProduct = (e) => {
+    setIsSearched(true)
+    setIsSuggestionVisible(false);
     setSearch(e.target.value);
   };
 
   const onKeyDownHandler = (key) => {
     if(!search) return;
-    if(key === "Enter") { 
-      searchRef.current.value = ""
-      setSearch("");
+    if(key === "Enter") {
+      setIsSearched(true)
+      const searchHistories = JSON.parse(localStorage.getItem('search-history'))|| [];
+      const currentSearch = {id: new Date().getTime(), name: search}
+
+      searchHistories.push(currentSearch);
+
+      setSearchHistory(searchHistories);
+      
+      localStorage.setItem('search-history', JSON.stringify(searchHistories))
+
+      // searchRef.current.value = ""
+
+      // setSearch(""); @todo 
+
       router.push(`/products?search=${encodeURIComponent(search)}`).then((_) => {
         window.scrollTo(0, 0);
       });
@@ -42,9 +80,11 @@ const SearchInput = () => {
 
   const searchClick = (searchData) => {
     if(!search) return;
-    searchRef.current.value = ""
+    setIsSearched(true)
+    // searchRef.current.value = ""
 
-    setSearch("");
+    // setSearch(""); @todo 
+
     setSearchType('product');
     dispatch(toggleBackdrop());
 
@@ -87,10 +127,19 @@ const SearchInput = () => {
   }
 
   useEffect(() => {
+    const getSearchHistory = JSON.parse(localStorage.getItem('search-history')) || [];
+  
+    setSearchHistory(getSearchHistory)
+
+  }, [JSON.stringify(searchHistory)])
+
+  useEffect(() => {
+
     if(firstRenderRef.current) {
       firstRenderRef.current = false
       return
     }
+
     const source = axios.CancelToken.source();
 
     dispatch(searchProductAction({search: search, type: searchType}, source));
@@ -104,18 +153,82 @@ const SearchInput = () => {
     }
   }, [search, searchType])
 
+  const inputFocusHandler = () => {
+    if(search) return;
+    setIsSearched(false)
+
+    if(searchHistory.length === 0) return;
+    
+    setIsSuggestionVisible(true);
+  }
+
+  const searchHistoryClickHandler = searchQuery => {
+    setSearch(searchQuery);
+    searchRef.current.value = searchQuery || ""
+  }
+
+  const toggleInputAction = () => {
+    if(isSuggestionVisible && !search) {
+      setIsSuggestionVisible(false)
+    }
+
+    if(search) {
+      setSearch("")
+      searchRef.current.value = ""
+    }
+  }
+
+  const removeSearchItem = (id) => {
+    const cloneSearchHistory = [...searchHistory];
+
+    const updatedSearchHistory = cloneSearchHistory.filter(searchItem => searchItem.id !== id)
+
+    setSearchHistory(updatedSearchHistory);
+
+    localStorage.setItem('search-history', JSON.stringify(updatedSearchHistory));
+  }  
+
   return (
     <>
       <input
         ref={el => searchRef.current = el}
         className="search-input"
         placeholder={translate("Search for Products, Brands or more")}
+        onFocus={inputFocusHandler}
+        // onBlur={() => setIsSuggestionVisible(false)}
         onChange={(e) => searchProduct(e)}
         onKeyDown={e => onKeyDownHandler(e.key)}
       />
+
+      <div style={{position: 'absolute', zIndex: '100', right: 'calc(63px + 15px)', top: '16px', fontSize: '12px'}}>
+        <span className="color-main pointer" onClick={toggleInputAction}>
+          {
+            (isSuggestionVisible && !search) ? 'close' : (search) && 'remove'
+          }
+          </span>
+      </div>
+
       <div className="header-custom-prepend pointer" onClick={() => onKeyDownHandler('Enter')} >
         <i className="fas fa-search"></i>
       </div>
+
+        {
+          isSuggestionVisible && !search && (
+            <div className="search-suggestion-area modal-scrollbar">
+              {
+                searchHistory && searchHistory.map((searchItem, index) => (
+                  <div className="py-3 px-2" key={index}>
+                    <div className="d-flex justify-content-between">
+                      <span className="pointer" onClick={() => {searchHistoryClickHandler(searchItem.name); setIsSuggestionVisible(false); setIsSearched(true); setTimeout(() => {searchRef.current.focus()}, 50);}}>{searchItem.name}</span>
+                      <span style={{fontSize: '12px'}} className="pointer color-main" onClick={() => removeSearchItem(searchItem.id)}>delete</span>
+                    </div>
+                  </div>
+                ))
+              }
+
+            </div>
+          )
+        }
 
       {search.length > 0 && (
         <div className="search-suggestion-area modal-scrollbar">
@@ -135,7 +248,7 @@ const SearchInput = () => {
           </div>
 
           {
-            search && loading && <SearchLoadingSkeleton/>
+            search && loading && isSearched && <SearchLoadingSkeleton/>
           }
 
           {
