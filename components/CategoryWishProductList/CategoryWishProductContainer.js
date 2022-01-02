4f470f082d92953f3ac1f9cd8fcb89a8056aaa83
hasibulhossain52@gmail.com <hasibulhossain52@gmail.com>
2022-01-02 17:15:10 +0600

    functional: update product filter system

--- components/CategoryWishProductList/CategoryWishProductContainer.js
+++ components/CategoryWishProductList/CategoryWishProductContainer.js
@@ -12,7 +12,6 @@ import {
 import {useRouter} from 'next/router'
 import Image from 'next/image';
 import Modal from "../master/Modal/Modal";
-import { getShopList } from "../Shop/_redux/Action/ShopAction";
 import Axios from 'axios'
 import Paginate from "../master/paginate/Paginate";
 import ImageWithFallback from '../master/Image/Image';
@@ -20,20 +19,16 @@ import Link from 'next/link';
 import { parseFilterString, parseUri } from "../../helper/parse-filter-query";
 
 const CategoryWishProductContainer = ({ isMainCategory, subCategories, mainCategoryBanner }) => {
-  const dispatch = useDispatch();
-  const router = useRouter();
-  const [showFilter, setShowFilter] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  // const [filterParam, setFilterParam] = useState(parseUri(router.query));
+  const [showFilter, setShowFilter]              = useState(false);
+  const [currentPage, setCurrentPage]            = useState(1);
+  const [filterParams, setFilterParamsFromQuery] = useState(null);
+  const dispatch                                 = useDispatch();
+  const router                                   = useRouter();
+  const {type: typeQuery = ""}                   = router.query;
 
-  const { paginate, filterParams, categoryBrandDetails, isLoading } = useSelector(
+  const { paginate, categoryBrandDetails, isLoading } = useSelector(
     (state) => state.CategoryWiseProductReducer
   );
-
-  // const firstRenderRef = useRef(true);
-
-
-  const {type: typeQuery = ""} = router.query;
 
 
   const paginateHandler = (page) => {
@@ -45,8 +40,6 @@ const CategoryWishProductContainer = ({ isMainCategory, subCategories, mainCateg
       pathname: '/products',
       query: parseFilterString(router.query, {page: page.selected + 1})
     })
-
-    // dispatch(setFilterParams(filterParamClone));
   };
 
   const getImgSrc = () => {
@@ -63,27 +56,47 @@ const CategoryWishProductContainer = ({ isMainCategory, subCategories, mainCateg
     return src;
   }
 
-
-
-
-  // useEffect(() => {
-  //   dispatch(getSubCategories(null))
-  // }, []);
-
-
   useEffect(() => {
     const source = Axios.CancelToken.source();
+    window.scrollTo({ top: 0, behavior: "smooth" }); // Scroll to top after mount
     const {filterParam, finalUriObj} = parseUri(router.query);
-    const currentPage = finalUriObj['page'] || 1
+
+    const currentPage = finalUriObj['page'] || 1;
+
     setCurrentPage(currentPage);
+    setFilterParamsFromQuery(finalUriObj);
+    
     dispatch(getFilteredProducts(filterParam, source));
 
-    window.scrollTo({ top: 0, behavior: "smooth" }); // Scroll to top after mount
-  
+    const category = router.query?.['category'] || null;
+
+    dispatch(getSubCategories(category));
+
+    const cloneQueries = { ...finalUriObj };
+
+    if(router.query?.['category']) {
+      dispatch(getCategoryOrBrandDetails('categories/' + router.query?.['category']));
+    }
+    
+    if(router.query?.['brand']) {
+      dispatch(getCategoryOrBrandDetails('brands/' + router.query?.['brand']));
+    }
+
+    cloneQueries['category'] = [null, cloneQueries?.['category']] || [];
+    cloneQueries['brand']    = cloneQueries?.['brand']?.split?.(',') || [];
+
+    dispatch(setFilterParams(cloneQueries));
+
     return () => {
       source.cancel()
     }
   }, [JSON.stringify(router.query)]);
+
+  useEffect(() => {
+    return () => {
+      dispatch(resetFilterParams(filterParams))
+    }
+  }, [])
 
 
 
@@ -100,7 +113,7 @@ const CategoryWishProductContainer = ({ isMainCategory, subCategories, mainCateg
       <section className="pt-2 pt-md-4">
         {
           (categoryBrandDetails.banner_url || typeQuery === 'haat-bazaar' || isMainCategory) && (
-              <div className="banner mb-3">
+              <div className="banner mb-md-2 px-1 px-md-3">
                 <div className="banner-photo-box">
                   <Image src={getImgSrc()} width={1260} height={280} />
                 </div>
@@ -112,7 +125,7 @@ const CategoryWishProductContainer = ({ isMainCategory, subCategories, mainCateg
           {
             !isMainCategory &&
             <div className="col-md-12 mb-5 px-0" style={{fontSize: '14px'}}>
-              <CategoryWishProductList showFilter={showFilter} showFilterHandler={() => setShowFilter(preState => !preState)} />
+              <CategoryWishProductList filterParams={filterParams} showFilter={showFilter} showFilterHandler={() => setShowFilter(preState => !preState)} />
               {
                 !isLoading && paginate.total > paginate.per_page  && (
                   <>
@@ -130,15 +143,15 @@ const CategoryWishProductContainer = ({ isMainCategory, subCategories, mainCateg
           }
           {
             isMainCategory &&
-            subCategories.map?.((item, index) => {
+            subCategories?.map?.((item, index) => {
               return (
-                <div className="col-lg-3 col-md-4 py-3" key={index}>
+                <div className="col-lg-3 col-md-4 col-6 py-2 py-md-3 px-1 px-md-3" key={index}>
                   <div className="pointer">
-                    <Link href={`/products?category=${encodeURIComponent(item.short_code)}&name=${encodeURIComponent(item.name)}&filter=paginate_no=40`}>
+                    <Link href={`/products?category=${encodeURIComponent(item.short_code)}&name=${encodeURIComponent(item.name)}&filter=paginate_no__40`}>
                       <a>
                         <div className="text-center">
                           <ImageWithFallback width={400} height={280} src={item?.image_url} alt={item?.name} />
-                          <span className="d-inline-block pt-2 color-secondary color-main-hover">
+                          <span className="d-inline-block pt-2 color-secondary color-main-hover font-15">
                             {
                               item?.name
                             }
@@ -157,156 +170,4 @@ const CategoryWishProductContainer = ({ isMainCategory, subCategories, mainCateg
   );
 };
 
-// brand, category, type, storeById, search
-
-let obj = {category: 'groceries', name: 'Groceries', filter: 'order_by__price--order__asc--paginate_no__100--page__1'};
-
-
 export default CategoryWishProductContainer;
-
-
-
-
-
-
-
-
-// const {
-//   search,
-//   category,
-//   brand,
-//   min_price,
-//   max_price,
-//   attributes,
-//   rating,
-//   paginate_no,
-//   order_by,
-//   seller_id,
-//   order,
-//   type,
-//   page
-// } = filterParams;
-
-// useEffect(() => {
-//   const queries = {}
-  
-//   if(router.query["storeById"]) {
-//     queries["storeById"] = router.query.storeById
-//   }
-  
-//   if(router.query["brand"]) {
-//     queries["brand"] = router.query.brand
-//   }
-  
-//   if(router.query["category"]) {
-//     queries["category"] = router.query.category
-//   }
-  
-//   if(router.query["type"]) {
-//     queries["type"] = router.query.type
-//   }
-  
-//   if(router.query["storeById"]) {
-//     queries["seller_id"] = router.query.storeById
-//   }
-  
-//   if(router.query["search"]) {
-//     queries["search"] = router.query.search
-//   }
-  
-//   const cloneFilterParams = {...filterParams};
-
-//   for(const query in queries) {
-//     if(Array.isArray(cloneFilterParams[query])) {
-//       // cloneFilterParams[query] = [];
-
-//       if(query === 'brand') {
-//         cloneFilterParams.seller_id = "";
-//         cloneFilterParams.search = "";
-//         cloneFilterParams.category = [];
-//         cloneFilterParams.page = 1;
-//         cloneFilterParams[query].push(queries[query]);
-
-//         dispatch(getCategoryOrBrandDetails('brands/' + queries[query]));
-//       }
-//       if(query === 'category') {
-//         cloneFilterParams.search = "";
-//         cloneFilterParams.seller_id = "";
-//         cloneFilterParams.brand = [];
-//         // check if category same or not after remount
-//         if(cloneFilterParams[query].length > 0 && !(cloneFilterParams[query][0] === queries[query])) {
-//           cloneFilterParams.page = 1;
-//           cloneFilterParams[query] = [];
-//           cloneFilterParams[query].push(queries[query]);
-//         } else {
-
-//           cloneFilterParams[query] = [];
-//           cloneFilterParams[query].push(queries[query]);
-//         }
-
-//         dispatch(getSubCategories(queries[query]))
-
-//         dispatch(getCategoryOrBrandDetails('categories/' + queries[query]));
-//       }
-
-//     } else {
-//       cloneFilterParams.category = [];
-//       cloneFilterParams.brand = [];
-//       cloneFilterParams.page = 1;
-//       cloneFilterParams.seller_id = "";
-
-//       if(query === 'seller_id') {
-//         cloneFilterParams[query] = queries[query]
-//         dispatch(getSubCategories(null))
-//       }
-//       if(query === 'type') {
-//         cloneFilterParams[query] = queries[query]
-//         dispatch(getSubCategories(null))
-//       }
-//       if(query === 'search') {
-//         cloneFilterParams[query] = queries[query]
-//         dispatch(getSubCategories(null))
-//       }
-//     }
-//   }
-//   dispatch(setFilterParams(cloneFilterParams));
-
-//   dispatch(getShopList());
-
-// }, [brandQuery, categoryQuery, typeQuery, storeById, searchQuery, mainCategory]);
-
-
-// useEffect(() => {
-//   return () => {
-//     dispatch(resetFilterParams(filterParams))
-//   }
-// }, [])
-
-
-// useEffect(() => {
-//   // if(firstRenderRef.current) {
-//   //   firstRenderRef.current = false
-//   //   return
-//   // }
-//   const source = Axios.CancelToken.source();
-//   dispatch(getFilteredProducts(filterParams, source));
-//   window.scrollTo({ top: 0, behavior: "smooth" }); // Scroll to top after mount
-
-//   return () => {
-//     source.cancel()
-//   }
-// }, [
-//   attributes,
-//   JSON.stringify(brand),
-//   JSON.stringify(category),
-//   max_price,
-//   min_price,
-//   rating,
-//   search,
-//   paginate_no,
-//   order_by,
-//   order,
-//   type,
-//   page,
-//   seller_id
-// ]);
