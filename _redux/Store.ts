76f181d4c1f30f63812817709e80308c9719ab13
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-01-16 12:47:42 +0600

    refactor: removed some unnecessary comments

--- _redux/Store.ts
+++ _redux/Store.ts
@@ -5,10 +5,8 @@ const Store = configureStore({
   reducer: RootReducer,
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof Store.getState>;
 
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof Store.dispatch;
 
 export default Store;
