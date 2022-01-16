c30f871d2d3c33bfc627664e35ec363687f03ed5
ManiruzzamanAkash <manirujjamanakash@gmail.com>
2022-01-16 12:30:24 +0600

    refactor: Refactored whole reducer to short names

--- _redux/RootReducer.ts
+++ _redux/RootReducer.ts
@@ -1,15 +1,14 @@
 import { combineReducers } from "redux";
 
 import GlobalReducer, { IGlobalReducer } from "./store/reducer/GlobalReducer";
-import CategoryWiseProductReducer from "../components/CategoryWishProductList/_redux/Reducer/CategoryWiseProductReducer";
+import HeaderReducer, { IHeaderReducer } from "../components/Header/_redux/HeaderReducer/HeaderReducer";
+import CategoryReducer from "../components/CategoryWishProductList/_redux/Reducer/CategoryReducer";
 import CompanyPolicyReducer from "../components/CompanyPolicy/_redux/Reducer/CompanyPolicyReducer";
 import DealFlashReducer from "../components/DealFlash/_redux/Reducer/DealFlashReducer";
-import HeaderReducer from "../components/Header/_redux/HeaderReducer/HeaderReducer";
 import HomeBannerCarouselReducer from "../components/homeBannerCarousel/_redux/homeBannerCarouselReducer/HomeBannerCarouselReducer";
 import ProductTopListReducer from "../components/ProductTopList/_redux/Reducer/ProductTopListReducer";
 import ShopReducer from "../components/Shop/_redux/Reducer/ShopReducer";
 import StoreReducer from "../components/store/_redux/reducer/store-reducer";
-import CategoryReducer from "../components/category/_redux/Reducer/CategoryReducer";
 import AuthReducer from "../components/LoginRegistration/_redux/Reducer/AuthReducer";
 import UserDataReducer from "../components/_redux/getUserData/Reducer/UserDataReducer";
 import ProfileAccountSettingReducer from "../components/ProfileAccountSetting/_redux/Reducer/ProfileAccountSettingReducer";
@@ -25,71 +24,85 @@ import ProductReviewReducer from "../components/product-review/_redux/reducer/pr
 import ShippingInfoReducer from "../components/ShippingInfo/_redux/Reducer/ShippingInfoReducer";
 import FooterReducer from "../components/Footer/_redux/Reducer/FooterReducer";
 import ReviewReducer from "../components/product-review/_redux/reducer/productReviewReducer";
-import ForgetPasswordReducer from '../components/forget-password/_redux/reducer/forget-password-reducer';
-import StoreInfoReducer from '../components/store-info/_redux/reducer/store-info_reducer';
+import ForgetPasswordReducer from "../components/forget-password/_redux/reducer/forget-password-reducer";
+import StoreInfoReducer from "../components/store-info/_redux/reducer/store-info_reducer";
 import NewOfferReducer from "../components/NewCollection/_redux/Reducer/new-offer-reducer";
 
+/**
+ * Reducer Interface definitions.
+ *
+ * @TODO: Need to implement interface for all of this reducers.
+ */
 export interface IRootReducer {
-  global                      : IGlobalReducer;
-  HeaderReducer               : object; // @TODO: Need to implement interface for all of this reducers.
-  HomeBannerCarouselReducer   : object;
-  ProductTopListReducer       : object;
-  CategoryReducer             : object;
-  CompanyPolicyReducer        : object;
-  DealFlashReducer            : object;
-  NewOfferReducer             : object;
-  CategoryWiseProductReducer  : object;
-  ShopReducer                 : object;
-  StoreReducer                : object;
-  StoreInfoReducer            : object;
-  AuthReducer                 : object;
-  UserDataReducer             : object;
-  ProfileAccountSettingReducer: object;
-  WishlistReducer             : object;
-  CartReducer                 : object;
-  SearchReducer               : object;
-  DeliveryInfoReducer         : object;
-  ProfileUpdateReducer        : object;
-  PaymentMethodReducer        : object;
-  ProductReducer              : object;
-  OrderReducer                : object;
-  ProductReviewReducer        : object;
-  ShippingInfoReducer         : object;
-  FooterReducer               : object;
-  ReviewReducer               : object;
-  ForgetPasswordReducer       : object;
+  global              : IGlobalReducer;
+  header              : IHeaderReducer;
+  sliders             : object;
+  companyPolicy       : object;
+  flashDeal           : object;
+  offer               : object;
+
+  shop                : object;
+  store               : object;
+  storeInfo           : object;
+
+  auth                : object;
+  user                : object;
+  userProfile         : object;
+  userAddress         : object;
+  authForgetPassword  : object;
+
+  wishlist            : object;
+  cart                : object;
+
+  category            : object;
+  product             : object;
+  productsTop         : object;
+  productSearch       : object;
+  productReview       : object;
+  productReviewAnother: object;
+
+  order               : object;
+  orderDelivery       : object;
+  OrderShipping       : object;
+
+  paymentMethod       : object;
+  footer              : object;
 }
 
 const reducers = {
-  global                      : GlobalReducer,
-  HeaderReducer               : HeaderReducer,
-  ProductTopListReducer       : ProductTopListReducer,
-  CategoryReducer             : CategoryReducer,
-  CompanyPolicyReducer        : CompanyPolicyReducer,
-  DealFlashReducer            : DealFlashReducer,
-  NewOfferReducer             : NewOfferReducer,
-  ShopReducer                 : ShopReducer,
-  StoreReducer                : StoreReducer,
-  StoreInfoReducer            : StoreInfoReducer,
-  AuthReducer                 : AuthReducer,
-  UserDataReducer             : UserDataReducer,
-  WishlistReducer             : WishlistReducer,
-  CartReducer                 : CartReducer,
-  SearchReducer               : SearchReducer,
-  DeliveryInfoReducer         : DeliveryInfoReducer,
-  ProfileUpdateReducer        : ProfileUpdateReducer,
-  PaymentMethodReducer        : PaymentMethodReducer,
-  ProductReducer              : ProductReducer,
-  OrderReducer                : OrderReducer,
-  ProductReviewReducer        : ProductReviewReducer,
-  ShippingInfoReducer         : ShippingInfoReducer,
-  FooterReducer               : FooterReducer,
-  ReviewReducer               : ReviewReducer,
-  ForgetPasswordReducer       : ForgetPasswordReducer,
-  HomeBannerCarouselReducer   : HomeBannerCarouselReducer,
-  CategoryWiseProductReducer  : CategoryWiseProductReducer,
-  ProfileAccountSettingReducer: ProfileAccountSettingReducer,
-};
+  global              : GlobalReducer,
+  header              : HeaderReducer,
+  sliders             : HomeBannerCarouselReducer,
+  companyPolicy       : CompanyPolicyReducer,
+  flashDeal           : DealFlashReducer,
+  offer               : NewOfferReducer,
 
+  shop                : ShopReducer,
+  store               : StoreReducer,
+  storeInfo           : StoreInfoReducer,
+
+  auth                : AuthReducer,
+  authForgetPassword  : ForgetPasswordReducer,
+  user                : UserDataReducer,
+  userProfile         : ProfileAccountSettingReducer,
+  userAddress         : ProfileUpdateReducer,
+
+  wishlist            : WishlistReducer,
+  cart                : CartReducer,
+
+  category            : CategoryReducer,
+  product             : ProductReducer,
+  productsTop         : ProductTopListReducer,
+  productSearch       : SearchReducer,
+  productReview       : ProductReviewReducer,
+  productReviewAnother: ReviewReducer,
+
+  order               : OrderReducer,
+  orderDelivery       : DeliveryInfoReducer,
+  OrderShipping       : ShippingInfoReducer,
+
+  paymentMethod       : PaymentMethodReducer,
+  footer              : FooterReducer,
+};
 
 export default combineReducers(reducers);
